84f4828eddb316bbb2d9c2827cd867725a4848eb
David Lindgren Kamali <david.lindgren.kamali@outlook.com>
2023-03-08 00:07:33 +0100

    Vecka 2 push 2

--- Vecka 2/dog.py
+++ Vecka 2/dog.py
@@ -1,11 +1,18 @@
+import json
+import os
+
+
 class Dog:
     def __init__(self, name, race, age, weight):
         self.name = name
         self.race = race
         self.age = age
         self.weight = weight
-        self.tailLength = self.getTailLength(self)
+        self.tailLength = self.getTailLength()
             
+    def to_dict(self):
+        return {"name": self.name, "race": self.race, "age": self.age, "weight":self.weight,
+                "tailLength": self.tailLength}
 
     def getTailLength(dog):
 
@@ -13,7 +20,7 @@ class Dog:
            return 3.7
 
         else:
-            return dog.age*dog.weight/10
+            return float(dog.age)*float(dog.weight)/10
     
     def __repr__(self):
         
@@ -29,20 +36,22 @@ class Kennel:
     def addDog(self):
         name = input("What's your dog's name:")
         race = input("What's your dog's race:")
-        age = input("What's your dog's age:")
-        weight = input("What's your dog's weight:")
+        age = int(input("What's your dog's age:"))
+        weight = float(input("What's your dog's weight:"))
 
         newDog = Dog(name, race, age, weight)
         self.dogs.append(newDog)
         print(Dog, "was added")
     
 
+
     def printDogs(self):
-        number = input("Min tail length: ")
+        
+        number = int(input("Min tail length: "))
         
         for d in self.dogs:
             if(d.tailLength >= number):
-                print(d)
+                print(d.name)
 
     def removeDog(self):
       name = input("Name of the dog to remove:")
@@ -53,7 +62,8 @@ class Kennel:
               print("%s was removed", name)
               break
 
-      print("No dog with name %s exists", name)        
+      print("No dog with name %s exists", name)       
+
 
 
 class System:
@@ -70,16 +80,38 @@ class System:
             print("3: Remove dog")
             print("4: Exit system")
 
-            number = input()
+            number = int(input())
 
             if(number == 1):
                 self.kennel.addDog()
+                self.save()
 
             elif(number == 2):
                 self.kennel.printDogs()
 
             elif(number == 3):
                 self.kennel.removeDog()
+                self.save()
 
             elif(number == 4):
                 self.running = False
+
+    def save(self):
+        with open("kennel.json", "w") as file:
+            dogDict = [dog.to_dict() for dog in self.kennel.dogs]
+            file.write(json.dumps({"dogs": dogDict}))
+
+    def read(self):
+        
+            with open("kennel.json", "r") as file:
+                kennel = json.load(file)
+                
+                for d in kennel["dogs"]:
+                    self.kennel.dogs.append(Dog(d["name"], d["race"], int(d["age"]), int(d["weight"])))
+        
+        
+
+s = System()
+s.read()
+
+s.run()
